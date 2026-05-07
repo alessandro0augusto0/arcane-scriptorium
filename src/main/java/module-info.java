dc62f78c0c72845c41720a5fde221d7cd468fec5
Alessandro Augusto <alessandro.guto1@gmail.com>
2026-05-06 23:46:16 -0300

    feat: fix restart, add logs, and polish JavaFX UI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.arcane.scriptorium {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.arcane.scriptorium to javafx.fxml;
+
     exports com.arcane.scriptorium;
 }
